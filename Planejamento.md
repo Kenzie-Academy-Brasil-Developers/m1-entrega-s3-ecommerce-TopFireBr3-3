687e9abeebfe9cfc6792cdda18c3800d944016cf
TopFireBr3 <87951872+TopFireBr3@users.noreply.github.com>
2022-03-23 16:18:13 -0300

    commit pages

---
+++ Planejamento.md
@@ -0,0 +1,58 @@
+Projeto Html
+    
+<!-- header -->
+1.Colocar logo
+2.implementação de uma "<nav>" como filho uma "<ul>" com suas "<li>(s)"
+<!-- section -->
+1.deve conter o "<main>" e o aside 
+<!-- Maim -->
+1.deve conter todos os cards
+<!-- Cards -->
+irá ter como padrão:
+1.uma "<figure>" e uma "<img>" contida 
+2.uma "<section>" que vai conter as tags: "<p>","<h3>","<span>","<h4>","<a>"
+<!-- Carrinho -->
+o carrinho terá um "<aside>" que irá ter:
+1.uma tag "<form>" como pai, e como seus filhos as tags:
+1."<input>"
+2."<button>"
+3."<h3>"
+2.uma tag "<div>" como pai, e como seus filhos as tags:
+1."<h2>"
+2."<a>"
+
+Projeto CSS
+<!-- tamanhos -->
+<!-- cores -->
+background
+1. Cor Primária: #27AE60
+2. Cor Secundária: #6B54DE
+Textos
+1. h1,h2,h3,h4,p: #333333
+2. span,a: rgb(198, 187, 173);
+
+Projeto JS
+<!-- obj -->
+criar objetos dos cards 
+
+<!-- function add -->
+faça uma function que adicone um item
+
+<!-- function remuve -->
+faça uma function que remova um item
+
+<!-- function menu -->
+faça uma function de filtro para o imput e para nav
+
+<!-- function carrinho-->
+faça uma function que re-crie os itens do carrinho
+1. link com a function add
+2. link com a function remuve
+
+<!-- function quantidade -->
+faça uma function que some todos os itens do carrinho
+1. link com a function carrinho
+   
+<!-- function Preco -->
+faça uma function que multiplique o preço de cada produto que está continho no card pela    
+
--- site 2/JS/JS.js
+++ site 2/JS/JS.js
@@ -4,7 +4,7 @@ const cards = [{
     preco: 100,
     descricao: "Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
     categoria: "Camisetas",
-    imagem: "./img/Men-Jacket-Front-Black__15466 1.png",
+    imagem: "/site 2/img/Men-Jacket-Front-Black__15466 1.png",
     filto: ["Camisetas", "R$100,00", "Adicione", "energia", "guarda-roupa", "inverno", "jaqueta", "vibrante", "preto", "1"]
 
 },
@@ -14,7 +14,7 @@ const cards = [{
     preco: 100,
     descricao: "O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...",
     categoria: "Acessórios",
-    imagem: "./img/image 1.png",
+    imagem: "/site 2/img/image 1.png",
     filto: ["Acessórios", "R$100,00", "gorro", "Black", "Next", "beldade", "bordado", "confortável", "preto", "2"]
 },
 {
@@ -23,7 +23,7 @@ const cards = [{
     preco: 40,
     descricao: "Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...",
     categoria: "Acessórios",
-    imagem: "./img/Surgical-Mask-Black__89554 1.png",
+    imagem: "/site 2/img/Surgical-Mask-Black__89554 1.png",
     filto: ["Acessórios", "R$40,00", "máscara", "facial", "durável", "camadas", "tecido", "presilhas", "black", "preto", "3", "Mask"]
 },
 {
@@ -32,7 +32,7 @@ const cards = [{
     preco: 100,
     descricao: "Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...",
     categoria: "Camisetas",
-    imagem: "./img/Men-TShirt-Black-Front__70046 1.png",
+    imagem: "/site 2/img/Men-TShirt-Black-Front__70046 1.png",
     filto: ["Camisetas", "preto", "camisetas", "R$100,00", "t-shirt", "guarda-roupa", "intemporal", "caimento", "black", "4"]
 },
 {
@@ -41,7 +41,7 @@ const cards = [{
     preco: 100,
     descricao: "Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...",
     categoria: "Camisetas",
-    imagem: "./img/mockup-a0dc2330__62146 1.png",
+    imagem: "/site 2/img/mockup-a0dc2330__62146 1.png",
     filto: ["preto", "camiseta", "camisetas", "básica", "guarda-roupa", "grosso", "R$100,00", "Short-Sleeve", "T-Shirt", "black", "5"]
 },
 {
@@ -50,7 +50,7 @@ const cards = [{
     preco: 100,
     descricao: "Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...",
     categoria: "Camisetas",
-    imagem: "./img/mockup-9b9894f1__67347 1.png",
+    imagem: "/site 2/img/mockup-9b9894f1__67347 1.png",
     filto: ["preto", "Protejer", "jaqueta", "Champion", "embalável", "poliéster", "camisetas", "R$100,00", "Packable", "black", "6"]
 }]
 
